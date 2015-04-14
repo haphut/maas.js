2c4a20a4fb36cf70890f3c3a3a01ccb39ce290a7
haphut <haphut@gmail.com>
2015-04-14 09:56:50 +0300

    Add gulpfile.js.

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var browserify = require('browserify');
+var exec = require('child_process').exec;
+var es6ify = require('es6ify');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var traceur = require('gulp-traceur');
+var rimraf = require('rimraf');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+
+gulp.task('clean', function(callback) {
+  rimraf('./lib', callback);
+});
+
+gulp.task('build', ['clean'], function() {
+  return gulp.src('src/**/*.js')
+    .pipe(traceur({modules: 'commonjs'}))
+    .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['src/**/*.js', 'test/**/*.js', 'gulpfile.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function(callback) {
+  return exec('./node_modules/.bin/mocha --compilers js:mocha-traceur',
+              function (err, stdout, stderr) {
+                console.log(stdout);
+                console.log(stderr);
+                callback(err);
+              });
+});
+
+gulp.task('browserify', ['build'], function() {
+  return browserify('./browserified/exports.js')
+    .transform(es6ify)
+    .bundle()
+    .pipe(source('maas.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./browserified/'));
+});
+
+gulp.task('default', function () {
+  gulp.start('browserify');
+});
