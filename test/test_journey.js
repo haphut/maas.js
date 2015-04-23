e870e08396dfba2962b8a2882f96a6fe3e44ee4a
haphut <haphut@gmail.com>
2015-04-23 15:30:45 +0300

    Add first tests.

---
+++ test/test_journey.js
@@ -0,0 +1,35 @@
+/**
+ * maas.js - Communicate with maas-server
+ * Copyright (C) 2015 haphut <haphut@gmail.com>
+ *
+ * This file is part of maas.js.
+ *
+ * maas.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * maas.js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with maas.js.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import should from 'should';
+
+import {createJourneyId} from '../src/journey';
+
+describe('createJourneyId', () => {
+  const id = createJourneyId();
+
+  it('returns a String', () => {
+    id.should.be.String;
+  });
+
+  it('returns something of length 36', () => {
+    id.should.have.length(36);
+  });
+});
